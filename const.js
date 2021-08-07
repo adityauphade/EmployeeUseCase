a716422729a01c8d12d4ea4667d4f374580fc5ed
Aditya Uphade <aditya.uphade@morningstar.com>
2021-08-08 00:37:52 +0530

    Index

---
+++ const.js
@@ -0,0 +1,10 @@
+let fulltime = 8;
+let parttime = 4;
+let rate = 20;
+let duration_days = 0;
+let empwage = 0;
+let total = 0;
+let maxdays = 20;
+let maxhours = 160;
+
+module.exports = {fulltime, parttime, rate, duration_days, empwage, total, maxdays, maxhours}
